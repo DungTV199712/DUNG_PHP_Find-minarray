5cc9cdb3b999e0f0521a43a6b99d7da604431a6b
TVDung <43330633+DungTV199712@users.noreply.github.com>
2018-10-30 11:48:12 +0700

    Add files via upload

---
+++ FindMin-array.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$a = [1,2,5,7,15,2,4,21];
+$Min = $a[0];
+    for($i=1;$i< count($a);$i++){
+        if($a[$i] < $Min){
+            $Min = $a[$i];
+        }
+    }
+    echo $Min;
+
+
+?>
+</body>
+</html>
